d3a9cf4a4e1cb4d690b6c2c039307412450de007
andreasjankarlsson <andreasjankarlsson@gmail.com>
2023-02-06 12:25:42 +0100

    ....

--- lab2.sql
+++ lab2.sql
@@ -51,9 +51,24 @@ VALUES
     ('Lunds bio',300);
 
 
-INSERT INTO MOVIE (IMDB_key,running_time,title,production_year)
+INSERT INTO Movie (IMDB_key,running_time,title,production_year)
 VALUES
     ('abcd123','90','Need for speed','2020'),
     ('efgh456','120','Avatar: The last airbender','2004'),
     ('ijkl789','116','Grabben i graven bredvid','2002'),
     ('mnop123','123','Alla vägar bär till rom','2002');
+
+INSERT INTO Performance (performance_date,performance_time,IMDB_key,theater_name)
+VALUES
+    ('2022–02-01','19:00','abcd123','Sparta'),
+    ('2022–02-01','19:30','abcd123','Sparta'),
+    ('2022–02-01','20:00','abcd123','Sparta'),
+    ('2022–02-01','20:30','abcd123','Sparta'),
+    ('2022–03-06','19:00','efgh456','Västgöta Nation'),
+    ('2022–03-06','19:30','efgh456','Västgöta Nation'),
+    ('2022–03-06','20:00','efgh456','Västgöta Nation'),
+    ('2022–03-06','20:30','efgh456','Västgöta Nation'),
+    ('2022–03-06','19:00','ijkl789','Lunds Bio'),
+    ('2022–03-06','19:30','ijkl789','Lunds Bio'),
+    ('2022–03-06','20:00','ijkl789','Lunds Bio'),
+    ('2022–03-06','20:30','ijkl789','Lunds Bio');
