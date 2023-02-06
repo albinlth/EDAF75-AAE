9de09f618412c3305862d77fda8af34531bfad4c
andreasjankarlsson <andreasjankarlsson@gmail.com>
2023-02-06 12:51:31 +0100

    push

--- lab2.sql
+++ lab2.sql
@@ -1,42 +1,55 @@
 
-DROP TABLE IF EXISTS theater
-CREATE TABLE theater (
-    theater_name TEXT PRIMARY KEY
+
+PRAGMA foreign_keys=OFF;
+DROP TABLE IF EXISTS theater;
+DROP TABLE IF EXISTS performance;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS ticket;
+DROP TABLE IF EXISTS customer;
+
+PRAGMA foreign_keys=ON;
+
+
+CREATE TABLE Theater (
+    theater_name TEXT PRIMARY KEY,
     capacity INT 
-)
+);
 
-DROP TABLE IF EXISTS performance
-CREATE TABLE performance (
-    performance_id TEXT PRIMARY KEY
-    performance_date DATE
-    start_time TIME
-
-    FOREIGN KEY(IMDB_key) REFERENCES movie(IMDB_key)
-    FOREIGN KEY(theater_name) REFERENCES theater(theater_name)     
-)
-
-DROP TABLE IF EXISTS movie
-CREATE TABLE movie (
-    IMDB_key TEXT PRIMARY KEY
-    running_time INT
-    title TEXT
+CREATE TABLE Movie (
+    IMDB_key TEXT PRIMARY KEY,
+    running_time INT,
+    title TEXT,
     production_year INT
-)
+);
 
-DROP TABLE IF EXISTS ticket
-CREATE TABLE ticket (
-    ticket_id lower(hex(randomblob(16))) PRIMARY KEY
-
-    FOREIGN KEY(performance_id) REFERENCES performance(performance_id)
-    FOREIGN KEY(username) REFERENCES customer(username)
-)
-
-DROP TABLE IF EXISTS customer
-CREATE TABLE customer (
-    username TEXT PRIMARY KEY
-    full_name TEXT
+CREATE TABLE Customer (
+    username TEXT PRIMARY KEY,
+    full_name TEXT,
     user_password TEXT
-)
+);
+
+
+CREATE TABLE Performance (
+    performance_id TEXT PRIMARY KEY,
+    performance_date DATE,
+    start_time TIME,
+    IMDB_KEY TEXT,
+    theater_name TEXT,
+
+    FOREIGN KEY (IMDB_key) REFERENCES Movie(IMDB_key),
+    FOREIGN KEY (theater_name) REFERENCES Theater(theater_name)
+
+    );
+
+CREATE TABLE Ticket (
+    ticket_id TEXT PRIMARY KEY,
+    performance_id TEXT,
+    username TEXT,
+    FOREIGN KEY (performance_id) REFERENCES Performance(performance_id),
+    FOREIGN KEY (username) REFERENCES Customer(username)
+);
+
+
 
 INSERT INTO Theater (theater_name, capacity)
 VALUES 
@@ -47,12 +60,12 @@ VALUES
 
 INSERT INTO Movie (IMDB_key,running_time,title,production_year)
 VALUES
-    ('abcd123','90','Need for speed','2020'),
-    ('efgh456','120','Avatar: The last airbender','2004'),
-    ('ijkl789','116','Grabben i graven bredvid','2002'),
-    ('mnop123','123','Alla vägar bär till rom','2002');
+    ('abcd123','90','Need for speed',2020),
+    ('efgh456','120','Avatar: The last airbender',2004),
+    ('ijkl789','116','Grabben i graven bredvid',2002),
+    ('mnop123','123','Alla vägar bär till rom',2002);
 
-INSERT INTO Performance (performance_date,performance_time,IMDB_key,theater_name)
+INSERT INTO Performance (performance_date,start_time,IMDB_key,theater_name)
 VALUES
     ('2022–02-01','19:00','abcd123','Sparta'),
     ('2022–02-01','19:30','abcd123','Sparta'),
@@ -66,3 +79,4 @@ VALUES
     ('2022–03-06','19:30','ijkl789','Lunds Bio'),
     ('2022–03-06','20:00','ijkl789','Lunds Bio'),
     ('2022–03-06','20:30','ijkl789','Lunds Bio');
+ 
