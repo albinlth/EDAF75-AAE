02dba0c837d2cea06c6c1b79bd5bf91c0c959477
albinlth <102159351+albinlth@users.noreply.github.com>
2023-02-06 12:11:35 +0100

    updated tables

--- lab2.sql
+++ lab2.sql
@@ -4,7 +4,7 @@ CREATE TABLE theater (
     theater_name TEXT
     capacity INT
 
-    PRIMARY KEY (theater_name)
+    PRIMARY KEY (theater_name) 
 )
 
 DROP TABLE IF EXISTS performance
@@ -12,14 +12,14 @@ CREATE TABLE performance (
     performance_id TEXT
     start_time DATETIME
 
-    FOREIGN KEY(IMDB_key) 
-    FOREIGN KEY(theater_name)
+    FOREIGN KEY(IMDB_key) REFERENCES movie(IMDB_key)
+    FOREIGN KEY(theater_name) REFERENCES theater(theater_name)
     PRIMARY KEY(performance_id)
      
 )
 
 DROP TABLE IF EXISTS movie
-CREATE TABLE movie(
+CREATE TABLE movie (
     IMDB_key TEXT
     running_time INT
     title TEXT
@@ -29,6 +29,17 @@ CREATE TABLE movie(
 )
 
 DROP TABLE IF EXISTS ticket
-CREATE TABLE ticket(
+CREATE TABLE ticket (
     ticket_id randomblob(16)
+    FOREIGN KEY(performance_id) REFERENCES performance(performance_id)
+    FOREIGN KEY(username) REFERENCES customer(username)
+)
+
+DROP TABLE IF EXISTS customer
+CREATE TABLE customer (
+    username TEXT
+    full_name TEXT
+    user_password TEXT
+
+    PRIMARY KEY(username)
 )
